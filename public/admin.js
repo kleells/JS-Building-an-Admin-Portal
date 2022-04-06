39ef9b7a649832d471611bd39c5d53e5e15e3bf0
Katie Eells <katieeells@Katies-MBP.lan>
2022-04-05 20:58:06 -0400

    first commit

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,43 @@
+async function bookData(){
 
-// Your Code Here
+    let response = await fetch ('http://localhost:3001/listBooks')
+
+    let bookList = await response.json()
+
+    bookList.forEach(bookTitles)
+}
+
+function bookTitles(book) {
+    let root = document.querySelector('#root')
+
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+// text input 
+    let bookQuantity = document.createElement('input')
+    bookQuantity.value = book.quantity
+    
+// Submit button
+    let submitBtn = document.createElement('button')
+    submitBtn.textContent = 'Submit'
+
+    submitBtn.addEventListener('click', (e) => {
+        fetch('http://localhost:3001/'), {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: bookQuantity.value
+            })
+        }
+    })
+
+
+    li.append(quantityInput, saveButton)
+
+    root.append(li)
+}
+
+bookData();
